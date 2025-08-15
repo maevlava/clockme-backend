036d9ae05b969195d588f2a0f7a85382532bf005
maevlava <maevlava@gmail.com>
2025-08-16 00:02:04 +0700

    feat: Implement time records seeder logic feat: Integrate time records seeder into main command

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -20,7 +20,7 @@ func main() {
 	logger.Init()
 	ctxBg := context.Background()
 
-	seedCmd := flag.String("seed", "all", "all, users, projects, tasks, or links")
+	seedCmd := flag.String("seed", "all", "all, users, projects, tasks, records, or links")
 	flag.Parse()
 
 	conn, queries := connectDB()
@@ -32,6 +32,7 @@ func main() {
 		seedProjects(queries, ctxBg)
 		seedProjectsUsers(queries, ctxBg, seedUsers(queries, ctxBg), seedProjects(queries, ctxBg))
 		seedTasks(queries, ctxBg)
+		seedTimeRecords(queries, ctxBg)
 		break
 	case "users":
 		seedUsers(queries, ctxBg)
@@ -40,6 +41,9 @@ func main() {
 		seedProjects(queries, ctxBg)
 	case "tasks":
 		seedTasks(queries, ctxBg)
+		break
+	case "records":
+		seedTimeRecords(queries, ctxBg)
 		break
 	case "links":
 		log.Info().Msg("Seeding links only. Fetching existing data...")
@@ -61,6 +65,12 @@ func main() {
 	}
 
 }
+
+type TaskInfo struct {
+	TaskID    uuid.UUID
+	ProjectID uuid.UUID
+}
+
 func connectDB() (*sql.DB, *db.Queries) {
 	ctxBg := context.Background()
 
@@ -199,8 +209,54 @@ func seedTasks(queries *db.Queries, ctxBg context.Context) []uuid.UUID {
 
 	return taskIDs
 }
-func getAllUserIDs(queries *db.Queries, ctx context.Context) ([]uuid.UUID, error) {
-	users, err := queries.GetAllUsers(ctx)
+func seedTimeRecords(queries *db.Queries, ctxBg context.Context) []uuid.UUID {
+	var timeRecordsIDs []uuid.UUID
+
+	tasksInfo, err := getAllTasksInfo(queries, ctxBg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Could not fetch tasks info")
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	now := time.Now()
+	tenDaysAgo := now.AddDate(0, 0, -10)
+	timeWindowInSeconds := now.Unix() - tenDaysAgo.Unix()
+	for i := 0; i < 15; i++ {
+		randomTaskInfoIndex := r.Intn(len(tasksInfo))
+		randomTaskInfo := tasksInfo[randomTaskInfoIndex]
+
+		randomOffsetInSeconds := r.Int63n(timeWindowInSeconds)
+		startTime := tenDaysAgo.Add(time.Second * time.Duration(randomOffsetInSeconds))
+
+		minDurationInSeconds := 5 * 60
+		maxDurationInSeconds := 6 * 60 * 60
+		randomDurationInSeconds := r.Intn(maxDurationInSeconds-minDurationInSeconds) + minDurationInSeconds
+		endTime := startTime.Add(time.Second * time.Duration(randomDurationInSeconds))
+		params := db.CreateTimeRecordParams{
+			ID:        uuid.New(),
+			Name:      faker.Sentence(),
+			StartTime: startTime,
+			EndTime:   endTime,
+			TaskID:    randomTaskInfo.TaskID,
+			ProjectID: randomTaskInfo.ProjectID,
+		}
+		newRecord, err := queries.CreateTimeRecord(ctxBg, params)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to create time record")
+			continue
+		} else {
+			log.Info().
+				Stringer("id", newRecord.ID).
+				Str("name", newRecord.Name).
+				Msg("Created time record")
+			timeRecordsIDs = append(timeRecordsIDs, newRecord.ID)
+		}
+	}
+	return timeRecordsIDs
+
+}
+func getAllUserIDs(queries *db.Queries, ctxBg context.Context) ([]uuid.UUID, error) {
+	users, err := queries.GetAllUsers(ctxBg)
 	if err != nil {
 		return nil, err
 	}
@@ -211,8 +267,8 @@ func getAllUserIDs(queries *db.Queries, ctx context.Context) ([]uuid.UUID, error
 	}
 	return userIDs, nil
 }
-func getAllProjectIDs(queries *db.Queries, ctx context.Context) ([]uuid.UUID, error) {
-	projects, err := queries.GetAllProjects(ctx)
+func getAllProjectIDs(queries *db.Queries, ctxBg context.Context) ([]uuid.UUID, error) {
+	projects, err := queries.GetAllProjects(ctxBg)
 	if err != nil {
 		return nil, err
 	}
@@ -222,4 +278,18 @@ func getAllProjectIDs(queries *db.Queries, ctx context.Context) ([]uuid.UUID, er
 		projectIDs[i] = p.ID
 	}
 	return projectIDs, nil
+}
+func getAllTasksInfo(queries *db.Queries, ctxBg context.Context) ([]TaskInfo, error) {
+	tasks, err := queries.GetAllTasks(ctxBg)
+	if err != nil {
+		return nil, err
+	}
+	taskInfo := make([]TaskInfo, len(tasks))
+	for i, t := range tasks {
+		taskInfo[i] = TaskInfo{
+			TaskID:    t.ID,
+			ProjectID: t.ProjectID,
+		}
+	}
+	return taskInfo, nil
 }
